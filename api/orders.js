17bc25f5be2bddfbe86425f9cbbd43c08a5b47df
emersoebf37-design <emersoebf37@gmail.com>
2026-05-24 17:41:41 -0300

    cimnmuit

--- api/orders.js
+++ api/orders.js
@@ -24,14 +24,18 @@ function getPrivateKey() {
   return `-----BEGIN PRIVATE KEY-----\n${lines}\n-----END PRIVATE KEY-----\n`;
 }
 
-if (!getApps().length) {
-  initializeApp({
-    credential: cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: getPrivateKey(),
-    }),
-  });
+try {
+  if (!getApps().length) {
+    initializeApp({
+      credential: cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: getPrivateKey(),
+      }),
+    });
+  }
+} catch(e) {
+  console.error("ERRO AO INICIALIZAR FIREBASE:", e.message);
 }
 
 const db = getFirestore();
