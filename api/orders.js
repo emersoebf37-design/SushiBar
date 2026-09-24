caa8d7531ce287c250c1d3cf84a7e2a5151229e9
Enzo Madruga Andrade da Fonseca <emersoebf37@gmail.com>
2026-09-24 20:09:12 -0300

    bug fix on yakisoba camarão, changed to yakisoba de camarão

--- api/orders.js
+++ api/orders.js
@@ -123,8 +123,8 @@ const PRODUCTS = {
   "Yakisoba de Carne (G)": 35,
   "Yakisoba Misto (M)": 30,
   "Yakisoba Misto (G)": 37,
-  "Yakisoba Camarão (M)":30,
-  "Yakisoba Camarão (G)":35,
+  "Yakisoba de Camarão (M)":30,
+  "Yakisoba de Camarão (G)":35,
 
   // ── Combos ──
   "Mega Combo Hot Roll": 40,
